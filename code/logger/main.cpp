fccaba1239173e7ea40a517da2beb1b52e55b8d7
Jason Taylor <jftaylor21@gmail.com>
2013-04-19 00:06:30 -0400

    screen based logging code written

--- code/logger/main.cpp
+++ code/logger/main.cpp
@@ -3,10 +3,20 @@
 
 int main()
 {
-  Utilities::Logger::get().error("Hello World");
-  Utilities::Logger::get().warning("Hello World");
-  Utilities::Logger::get().info("Hello World");
-  Utilities::Logger::get().fatal("Hello World");
+  Utilities::Logger::get().setMaxLogLevel(Utilities::Logger::LOGLEVEL_ERROR);
+  std::cout << "screen disabled" << std::endl;
+  Utilities::Logger::get().error("Should not see this\n");
+  std::cout << "screen enabled max level error" << std::endl;
+  Utilities::Logger::get().setScreenEnable(true);
+  Utilities::Logger::get().error("Hello World\n");
+  Utilities::Logger::get().warning("Should not see this\n");
+  Utilities::Logger::get().info("Should not see this\n");
+  Utilities::Logger::get().setMaxLogLevel(Utilities::Logger::LOGLEVEL_INFO);
+  std::cout << "screen enabled no max level" << std::endl;
+  Utilities::Logger::get().error("Hello World\n");
+  Utilities::Logger::get().warning("Hello World\n");
+  Utilities::Logger::get().info("Hello World\n");
+  Utilities::Logger::get().fatal("Hello World\n");
   std::cout << "You should not see this text" << std::endl;
   return 0;
 }
